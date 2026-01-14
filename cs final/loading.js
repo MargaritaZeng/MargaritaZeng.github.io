daf107fb65a43c7c50bff9785d06b2f11f0222de
MargaritaZeng <zengmargarita@gmail.com>
2026-01-13 19:32:08 -0600

    Jan 12 and 13
    finish the story part and show the image of the story part

--- cs final/loading.js
+++ cs final/loading.js
@@ -1,49 +1,74 @@
 // all the loading things and the name of the loading iamge 
-//name of the starting page 
-let mainimg ;
-let gamename ;
-let illustration ;
-let propBack ;
-let settingButton ;
-let startGameButton ;
 
-// let the button highlight 
-let illustrationHL ;
-let propBackHL ;
-let settingButtonHL ;
-let startGameButtonHL ;
+ 
+
+//name of the starting page img
+let mainimg ;              
+let gamename ;            // let the button highlight of start page 
+let illustration ;        let illustrationHL ;
+let propBack ;            let propBackHL ;
+let settingButton ;       let settingButtonHL ;
+let startGameButton ;     let startGameButtonHL ;
 
 //img of the mainthing in buttons
 let backimg;
 let bagimg;
-let illBackimg;
-let buttonimg;
-let backbuttonimg;
-let setbuttonimg;
-let illustrationimg;
+let illBackimg;           //img of the mainthing in buttons hight light thing 
+let buttonimg;            let buttonimgHL;
+let backbuttonimg;        let illustrationimgHL;
+let setbuttonimg;         let progreimgHL;
+let illustrationimg;      let backHomeimgHL;
 let progreimg;
 let backHomeimg;
-
-//img of the mainthing in buttons hight light thing 
-let buttonimgHL;
-let illustrationimgHL;
-let progreimgHL;
-let backHomeimgHL;
 
 // img of the progress 
 let proFirst ;
 
+// img of the gaming part setting 
+let bagG;                 let bagGHL; 
+let guideG;               let guideGHL;
+let buttonG;              let buttonGHL;
+let returnG;              let returnGHL;
+let progressG;            let progressGHL;
+let setButtonG;           let setButtonGHL;
+
+// the image of the plot part world box
+let textBox;
+let headBoxL;             let headBoxR;
+let nameBoxL;             let nameBoxR;
+
+// the image of the people pace
+let nemorin;
+let npc;
+let theCat;
+let theCatP;
+let serenie;
+let vallerin;
+
+// the mood of character
+let smile;
+let sad;                 let sadC;
+let happy;               let happyC;
+let impatient;           let impatientC;
+let speechless;          let speechlessC;
+let confuse;             let confuseC;
+let surprise;            let surpriseC
+let cry;
+let frown;
+
+
 
 // load img of the starting page img
-
 async function startpageImg(){
+//--------------------startpage img of buttons----------------------------
   mainimg = await loadImage("rpggameImg/startpage/backpage.png");
   gamename = await loadImage("rpggameImg/startpage/gamename.png");
   illustration = await loadImage("rpggameImg/startpage/illustration Bu.png");
   propBack = await loadImage("rpggameImg/startpage/prop back Bu.png");
   settingButton = await loadImage("rpggameImg/startpage/setting Bu.png");
   startGameButton = await loadImage("rpggameImg/startpage/start game Bu.png");
-//--------------------startpage img of main buttons----------------------------
+
+//--------------------main page img of button and back img--------------------
   backimg = await loadImage("rpggameImg/initial functions/backimg.png");
   bagimg = await loadImage("rpggameImg/initial functions/bagimg.png");
   illBackimg = await loadImage("rpggameImg/initial functions/illustration backimg.png");
@@ -54,21 +79,84 @@ async function startpageImg(){
   illustrationimg = await loadImage("rpggameImg/initial functions/illustration Bu.png");
   progreimg = await loadImage("rpggameImg/initial functions/task progre Bu.png");
   backHomeimg = await loadImage("rpggameImg/initial functions/backhome Bu.png");
-//--------------------main page img of button and bag img--------------------
-  proFirst = await loadImage("rpggameImg/gaming page/task progress updated/firstpage.png")
+
+// -----------------------the seting in the game ------------------
+  bagG = await loadImage("rpggameImg/gaming page/setting/bag Bu.png");
+  guideG = await loadImage("rpggameImg/gaming page/setting/beginner's guide Bu.png");
+  buttonG = await loadImage("rpggameImg/gaming page/setting/button setting Bu.png");
+  returnG = await loadImage("rpggameImg/gaming page/setting/return home Bu.png");
+  progressG = await loadImage("rpggameImg/gaming page/setting/task progress Bu.png");
+  setButtonG = await loadImage("rpggameImg/gaming page/setting/setting button.png");
 
 }
 
-async function startpageImgHL(){
+async function allImgHL(){
+//-----------------startpage img of the hight light buttton--------------
   illustrationHL = await loadImage("rpggameImg/startpage/illustration HL.png");
   propBackHL = await loadImage("rpggameImg/startpage/prop back HL.png");
   settingButtonHL = await loadImage("rpggameImg/startpage/setting HL.png");
   startGameButtonHL = await loadImage("rpggameImg/startpage/start game HL.png");
-//-----------------startpage img of the hight light buttton-----------------------
 
+  //-------------main page high light button----------------------
   illustrationimgHL = await loadImage("rpggameImg/initial functions/illustration HL.png");
   progreimgHL = await loadImage("rpggameImg/initial functions/task progre HL.png");
   backHomeimgHL = await loadImage("rpggameImg/initial functions/backhome HL.png");
   buttonimgHL = await loadImage("rpggameImg/initial functions/button setting HL.png");
+
+  //-------------------the hight light button img of the setting in the game------------------------------------------------------
+  bagGHL = await loadImage("rpggameImg/gaming page/setting/bag HL.png")
+  guideGHL = await loadImage("rpggameImg/gaming page/setting/beginners guide HL.png")
+  buttonGHL = await loadImage("rpggameImg/gaming page/setting/button setting HL.png")
+  returnGHL = await loadImage("rpggameImg/gaming page/setting/return home HL.png")
+  progressGHL = await loadImage("rpggameImg/gaming page/setting/task progress HL.png")
 }
 
+
+// the img of the game part  -----------------------------------
+async function gamingImg(){
+    //the story box
+    textBox = await loadImage("rpggameImg/gaming page/text box.png");
+    headBoxL = await loadImage("rpggameImg/gaming page/boxL.png");
+    headBoxR = await loadImage("rpggameImg/gaming page/boxR.png");
+    nameBoxL = await loadImage("rpggameImg/gaming page/name boxL.png");
+    nameBoxR = await loadImage("rpggameImg/gaming page/name boxR.png");
+
+    // the face of people
+    nemorin = await loadImage("rpggameImg/gaming page/portrait/nemorin.png");
+    npc = await loadImage("rpggameImg/gaming page/portrait/npc.png");
+    theCat = await loadImage("rpggameImg/gaming page/portrait/the catC.png");
+    theCatP = await loadImage("rpggameImg/gaming page/portrait/the catP.png");
+    serenie = await loadImage("rpggameImg/gaming page/portrait/serenie.png");
+    vallerin = await loadImage("rpggameImg/gaming page/portrait/vallerin.png");
+
+    // the progress img
+    proFirst = await loadImage("rpggameImg/gaming page/task progress updated/firstpage.png")
+
+    //the mood img
+    smile = await loadImage("rpggameImg/gaming page/expression/smile.png")
+    sad = await loadImage("rpggameImg/gaming page/expression/sad.png")
+    happy = await loadImage("rpggameImg/gaming page/expression/happy.png")
+    impatient = await loadImage("rpggameImg/gaming page/expression/impatirnt.png")
+    speechless = await loadImage("rpggameImg/gaming page/expression/speechless.png")
+    confuse = await loadImage("rpggameImg/gaming page/expression/confuse.png")
+    surprise = await loadImage("rpggameImg/gaming page/expression/surprise.png")
+    cry = await loadImage("rpggameImg/gaming page/expression/cry.png")
+    frown = await loadImage("rpggameImg/gaming page/expression/frown.png")
+
+    sadC = await loadImage("rpggameImg/gaming page/expression/sadC.png")
+    happyC = await loadImage("rpggameImg/gaming page/expression/happyC.png")
+    impatientC = await loadImage("rpggameImg/gaming page/expression/impatientC.png")
+    speechlessC = await loadImage("rpggameImg/gaming page/expression/speechless.png")
+    confuseC = await loadImage("rpggameImg/gaming page/expression/confuseC.png")
+    surpriseC = await loadImage("rpggameImg/gaming page/expression/surpriseC.png")
+}
+
+// let smile;
+// let sad;                 let sadC;
+// let happy;               let happyC;
+// let impatient;           let impatientC;
+// let speechless;          let speechlessC;
+// let confuse;             let confuseC;
+// let surprise;            let surpriseC
+// let cry;
+// let frown;
--- cs final/sketch.js
+++ cs final/sketch.js
@@ -4,6 +4,26 @@
 
 //check the gamestatus in the game 
 let gameStatus = "startpage" ;
+// startpage mainpage inbutton plot
+
+
+//this is to check the highlight button of starting bage 
+let highlightButton = 1;
+// this is to check the main page hight light button
+let highlightButtonM = 1;
+// this is to check the setting in the game  hight light button
+let highlightButtonG = 1;
+
+let settingS = 1;
+let settingG = "closed"; 
+// closed is setting are not show 
+// open is setting is show and closed
+// settingS%2 === 1 is setting is show and closed
+// inside is setting open the button
+//this is in the game part
+
+let storyList = 130 ;
+
 // --------------------------------seting -----------------------------
 
 
@@ -12,40 +32,70 @@ function setup() {
 
   // set the startpage Img and high light button 
   startpageImg();
-  startpageImgHL();
-  
+  allImgHL();
+  gamingImg();  
 }
 
 function draw() {
   background(220);
   //img 3:2
   // always the backround
-  
+  image(backimg, 0, 0, 1200, 800);
   //this is for starting bage 
 
   if (gameStatus === "startpage"){
     //this is the start page img 
     showStartingpage();
+
+    if(highlightButton===1) image(startGameButtonHL, 0, 0, 1200, 800);
+    else if(highlightButton===2) image(settingButtonHL, 200, 150, 1200, 800);
+    else if(highlightButton===3) image(illustrationHL, 0, 0, 1200, 800);
+    else if(highlightButton===4) image(propBackHL, 0, 0, 1200, 800);
   }
 
   //this is Highlight the start page button 
-  mainbuttonHL();
+
 //-----------startpage-----------
 
-  if(gameStatus === "mainpage" ){
-    image(backimg, 0, 0, 1200, 800);
+  if(gameStatus === "mainpage" ){    
     if(highlightButton===2) settingUp();
     else if(highlightButton=== 3) showImg();
     else if(highlightButton=== 4) propBag();
   }
 
   if(gameStatus === "inButton" ){
-    image(backimg, 0, 0, 1200, 800);
     if(highlightButton===2) settingUp();
     else if(highlightButton=== 3) showImg();
     else if(highlightButton=== 4) propBag();
   }
-}
+
+
+// ----- start the game -----plot part -----------
+  
+
+  if(settingG === "inside"){
+    if(highlightButtonG === 1) propBag();
+    else if(highlightButtonG === 2) settingUp();
+    else if(highlightButtonG === 3){
+      gameStatus = "startpage";
+      settingS++;
+    } 
+    else if(highlightButtonG === 5)showImg();  
+  }
+
+  if(settingG === "open"){
+    //show the setting buttonS
+    image(setButtonG, 0, -30, 1200, 800);
+    settingInside();
+  } 
+
+  if(gameStatus === "plot"){  
+    plotFirst();
+    
+  }
+
+ }
+  
 //--------------------------------setup and drawing ---------------------------
 
 //show the page of the starting page 
@@ -58,30 +108,8 @@ function showStartingpage(){
   image(startGameButton , 0, 0, 1200, 800);
 }
 
-
-
-// show the high light button
-function mainbuttonHL(){
-
-  //this is the starting page showing the highlight button
-  if(gameStatus === "startpage"){
-    if(highlightButton===1) image(startGameButtonHL, 0, 0, 1200, 800);
-    if(highlightButton===2) image(settingButtonHL, 200, 150, 1200, 800);
-    if(highlightButton===3) image(illustrationHL, 0, 0, 1200, 800);
-    if(highlightButton===4) image(propBackHL, 0, 0, 1200, 800);
-  }
-}
-
-//this is to check the highlight button of starting bage 
-let highlightButton = 1;
-// this is to check the main page hight light button
-let highlightButtonM = 1;
-
-
-
-
 function keyPressed(){
-  //this is the starting page keycode
+//this is the starting page keycode
   if (gameStatus === "startpage"){
     if (key === "w" || keyIsDown(38)){
       if (highlightButton === 4){
@@ -96,9 +124,15 @@ function keyPressed(){
       else highlightButton--;
     }
     if(key === " "){
-      gameStatus = "mainpage";
+      if(highlightButton === 1){
+        gameStatus = "plot";
+        settingG = "open"
+      }
+      else gameStatus = "mainpage";
     }
   }
+
+//------this is the mainpage keycode------------------------------
   else if(gameStatus === "mainpage"){
     if (key === "w" || keyIsDown(38)){
       highlightButtonM++;
@@ -110,14 +144,48 @@ function keyPressed(){
       if(highlightButtonM === 1){
         gameStatus = "startpage";
       }
-      else{
-        gameStatus = "inButton";
-      }
+      else gameStatus = "inButton";
     }
   }
+
+//------this is the main page key code ----------------------------------
   else if(gameStatus === "inButton"){
     if(key === " ") gameStatus ="mainpage";
   }
+
+  else if(settingG === "inside"){
+    if(key === " ") settingG = "open" ;
+  }
+//-------this is the setting in game's keycode-------------------------------------
+  else if(settingG === "open"){
+    // open the setting in game 
+    if(keyIsDown(16)){
+      settingS++;
+    }
+
+    //setting in game 
+    else if(settingS%2 === 0){
+      if (key === "w" || keyIsDown(38)){
+        if (highlightButtonG === 1) highlightButtonG = 5;
+        else highlightButtonG--;
+      } 
+      if (key === "s" || keyIsDown(40)){
+        if (highlightButtonG === 5)highlightButtonG = 1;
+        else highlightButtonG++;
+      }
+      if(key === " ") settingG = "inside";
+    }
+
+    //-------this is the story part keycode------------------------------
+    else if(gameStatus === "plot"){
+      if(key === " "){
+        storyList++ ;
+      }
+    }
+    
+  }
+
+
 
 }
 
@@ -140,24 +208,30 @@ function settingUp(){
     else if(highlightButtonM===2) image(buttonimgHL, 0, 100, 1200, 800);
 
   }
-  //in the button img
-  else if(gameStatus === "inButton"){
+
+  //in the button img and the setting in game's img
+  else if(gameStatus === "inButton" || settingG === "inside"){
     image(setbuttonimg, 0, 0, 1200, 800);
     image(backHomeimgHL, 100, -50, 1200, 800);
   }
+
 }
+
 // the bag in main page
 function propBag(){
-  if(gameStatus === "mainpage"){
+  if(gameStatus === "mainpage" || settingG === "inside"){
     image(bagimg, 0, 0, 1200, 800);
     //return home img
     image(backHomeimgHL, 100,-50, 1200, 800);
     if(highlightButtonM>1 || highlightButtonM<1) highlightButtonM = 1;
   }
+
 }
+
 //show img in the main page 
 function showImg(){
   if(gameStatus === "mainpage"){
+    //main page button 
     image(backbuttonimg, 0, 0, 1200, 800);
     image(illustrationimg, 0, 0, 1200, 800);
     image(progreimg, 0, 0, 1200, 800);
@@ -175,18 +249,135 @@ function showImg(){
   }
 
   else if(gameStatus === "inButton"){
+    // show the img 
 
-
-    if(highlightButtonM===3){
-      image(illBackimg, 0, 0, 1200, 800);
-      image(backHomeimgHL, 100,-50, 1200, 800);
-    }
-    else{
+    if(highlightButtonM===2){
+      // show the progress in game and startpage
       image(proFirst, 0, 0, 1200, 800);
       image(backHomeimgHL, 100,-50, 1200, 800);
     }
+    else{
+      image(illBackimg, 0, 0, 1200, 800);
+      image(backHomeimgHL, 100,-50, 1200, 800);     
+    }
   }
+
+  else if (settingG === "inside"){
+    // show the progress in game and startpage
+    image(proFirst, 0, 0, 1200, 800);
+    image(backHomeimgHL, 100,-50, 1200, 800);
+  }
+
 }
 
 
-// to start the game 
+// -------------------------to start the game ----------------------------------------
+
+
+// set the setting in the game 
+function settingInside(){
+  //let the setting always be close at first 
+  if(settingS%2 === 0){
+    // show the setting button 
+    image(bagG, 0, 0, 1200, 800);
+    image(guideG, 0, 0, 1200, 800);
+    image(buttonG, 0, 0, 1200, 800);
+    image(returnG, 0, 0, 1200, 800);
+    image(progressG, 0, 0, 1200, 800);
+
+    //show the hight light button in the seting 
+    if(highlightButtonG === 1)image(bagGHL, 0, 0, 1200, 800);
+    else if(highlightButtonG === 2)image(buttonGHL, 0, 0, 1200, 800);
+    else if(highlightButtonG === 3)image(returnGHL, 0, 0, 1200, 800);
+    else if(highlightButtonG === 4)image(guideGHL, 0, 0, 1200, 800);
+    else if(highlightButtonG === 5)image(progressGHL, 0, 0, 1200, 800);
+  }
+  
+}
+
+
+// strat the story in the first fart 
+function plotFirst(){
+
+  // show the box 
+  image(textBox, 0, 0, 1200, 800);
+  // image(headBoxL, 0, 0, 1200, 800);
+  // image(headBoxR, 0, 0, 1200, 800);
+  //----
+  
+  fill(217, 137, 52);
+  if(conversation[storyList][0] === "the cat"){
+    image(headBoxL, 0, 0, 1200, 800);
+    image(nameBoxL, 0, 0, 1200, 800);
+
+    if(storyList > 82) image(theCatP, 0, -100, 1300, 900);
+    else image(theCat, 0, -100, 1300, 900);
+
+    text(conversation[storyList][0],275, 602);
+  }
+
+  else if(conversation[storyList][0] === "nemorin"){
+    image(headBoxR, 0, 0, 1200, 800);
+    image(nameBoxR, 0, 0, 1200, 800);   
+    image(nemorin, 0, 0, 1200, 800);
+
+    text(conversation[storyList][0],830, 602);
+  }
+
+  else if(conversation[storyList][0] === "elderly woman"){
+    image(headBoxR, 0, 0, 1200, 800);
+    image(nameBoxR, 0, 0, 1200, 800);
+    image(serenie, 0, 0, 1200, 800);
+    textSize(20);
+    text(conversation[storyList][0],818, 605);
+  }
+
+  else if(conversation[storyList][0] === "sereniel"){//93
+    image(headBoxR, 0, 0, 1200, 800);
+    image(nameBoxR, 0, 0, 1200, 800);
+    image(serenie, 0, 0, 1200, 800);
+
+    text(conversation[storyList][0],830, 602);
+  }
+
+  
+
+  else if(conversation[storyList][0] === "receptionist"){   
+    image(headBoxR, 0, 0, 1200, 800);
+    image(nameBoxR, 0, 0, 1200, 800);
+    image(npc, 0, 0, 1200, 800);
+
+    textSize(20);
+    text(conversation[storyList][0],825, 602);
+  }
+
+  else if(conversation[storyList][0] === "vallerin"){    
+    image(headBoxR, 0, 0, 1200, 800);
+    image(nameBoxR, 0, 0, 1200, 800);
+    image(vallerin, 0, 0, 1200, 800);
+
+    text(conversation[storyList][0],830, 602);
+  }
+
+  else if(conversation[storyList][0] === "narrator"){
+    image(nameBoxL, 0, 0, 1200, 800);
+
+    text(conversation[storyList][0],255, 602);
+  }
+
+  //print the name
+
+  //print the conversation
+  fill(0);
+  textSize(30);
+  text(conversation[storyList][2],240, 675, 720, 200);
+  //fill(217, 137, 52, 50)
+  //rect(220, 605, 720, 200);
+}
+
+// let nemorin;
+// let npc;
+// let theCat;
+// let theCatP;
+// let serenie;
+// let vallerin;
